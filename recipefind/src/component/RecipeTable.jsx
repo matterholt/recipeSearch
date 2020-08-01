236dda350fbef929226d74317aefc9f7f033fad9
MAtterholt <matterholt84@gmail.com>
2020-08-01 11:33:56 -0400

    build basic table for recipes

---
+++ recipefind/src/component/RecipeTable.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+// const recipeObject____temp = (
+//     <li key={recipeInfo.id} className="recipe__card">
+//         <h2>{recipeInfo.title}</h2>
+//         <p>Time:{recipeInfo.readyInMinutes} mins</p>
+//         <div><img src={recipeInfo.image} width="150px" /></div>
+//         <div dangerouslySetInnerHTML={{ __html: recipeInfo.summary }} />
+//     </li>
+// )
+const RecipeHeader = () => {
+    return (
+        <TableHead>
+            <TableRow>
+                <TableCell>Add</TableCell>
+                <TableCell>Title</TableCell>
+                <TableCell>Image</TableCell>
+                <TableCell>Summary</TableCell>
+                <TableCell>Detail</TableCell>
+            </TableRow>
+        </TableHead>
+    )
+}
+
+const RecipeRows = ({ recipeItem }) => {
+    return (
+        <TableRow>
+            <TableCell>Add</TableCell>
+            <TableCell>{recipeItem.title}</TableCell>
+            <TableCell><img src={recipeItem.image} width="50px" alt={`recipeItem.title`} /></TableCell>
+            <TableCell>{<span dangerouslySetInnerHTML={{ __html: recipeItem.summary }}>
+            </span>}</TableCell>
+            <TableCell>Detail</TableCell>
+        </TableRow>
+    )
+}
+
+const RecipeTable = ({ recipeList }) => {
+    return (
+        <TableContainer>
+
+            <Table>
+                <RecipeHeader />
+                <TableBody>
+                    {recipeList.map((recipeData) => <RecipeRows recipeItem={recipeData} />)}
+                </TableBody>
+
+            </Table>
+        </TableContainer>
+
+    )
+}
+
+export default RecipeTable
