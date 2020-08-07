b5f8e8bfaeb2f1bd4312ae2d47ea5310c8c10c44
MAtterholt <matterholt84@gmail.com>
2020-08-07 15:54:18 -0400

    fetching api improved,

--- recipefind/src/App.js
+++ recipefind/src/App.js
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 import FetchingData from './component/FetchingData'
 import WeeklyRecipeList from './component/WeeklyRecipeList'
@@ -8,26 +9,43 @@ import RandomRecipeContainer from './component/RandomRecipeContainer'
 
 
 const reciKey = process.env.REACT_APP_KEY
-const APIPath = `https://api.spoonacular.com/recipes/random?apiKey=${reciKey}&number=12`
-
-
-
+const tempDataDesign = {
+  title: "Grilled Chicken Banh Mi",
+  summary: "You can never have too many Vietnamese recipes, so give Grilled Chicken Banh Mi a try. Watching your figure? This dairy free recipe has <b>481 calories</b>, <b>28g of protein</b>, and <b>14g of fat</b> per serving. For <b>$2.6 per serving</b>, this recipe <b>covers 24%</b> of your daily requirements of vitamins and minerals. This recipe serves 6. 43 people have tried and liked this recipe. It works best as a main course, and is done in around <b>around 45 minutes</b>. If you have sub rolls, chicken breasts, rice vinegar, and a few other ingredients on hand, you can make it. It will be a hit at your <b>The Fourth Of July</b> event. It is brought to you by Foodista. With a spoonacular <b>score of 69%</b>, this dish is solid. If you like this recipe, take a look at these similar recipes: <a href=\"https://spoonacular.com/recipes/grilled-chicken-bnh-m-41554\">Grilled Chicken Bánh Mì</a>, <a href=\"https://spoonacular.com/recipes/grilled-chicken-banh-mi-41528\">Grilled Chicken Banh Mi</a>, and <a href=\"https://spoonacular.com/recipes/grilled-chicken-banh-mi-41560\">Grilled-Chicken Banh Mi</a>.",
+  id: 645634,
+  image: "https://spoonacular.com/recipeImages/645634-556x370.jpg",
+  imageType: "jpg",
+  dishTypes: [
+    "lunch",
+    "main course",
+    "main dish"
+  ]
+}
 function App() {
   const [isFetching, setIsFetching] = useState(false)
+  const [isError, setIsError] = useState({ status: false, msg: '' })
   const [foodList, updateFoodList] = useState([])
   const [selectedFood, updateSelectedFood] = useState([])
   const [recipesLiked, addRecipeLiked] = useState([])
   const [recipesSaved, addRecipeSaved] = useState([])
 
-
+  useEffect(() => { console.log(foodList) }, [foodList])
   function wklyRandomRecipe() {
-    updateFoodList([])
-    setIsFetching(true)
-    fetch(APIPath)
-      .then((response) => response.json())
-      .then((data) => updateFoodList(data.recipes))
-      .catch((e) => console.log(e))
-    setIsFetching(false)
+    const quantity = 12 - foodList.length
+    const APIPath = `https://api.spoonacular.com/recipes/random?apiKey=${reciKey}&number=${quantity}`
+    const fetchRecipes = async () => {
+      try {
+        setIsFetching(true)
+        const response = await axios.get(APIPath);
+        let recipeListFetched = response.data.recipes
+        let compiledFoodList = [...foodList, recipeListFetched]
+        updateFoodList(compiledFoodList.flat())
+        setIsFetching(false)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fetchRecipes()
   }
   function removeSelectItemMainList(selectedItem) {
     const removedItem = foodList.filter(item => item.id !== selectedItem.id)
@@ -42,17 +60,20 @@ function App() {
     addRecipeLiked([...recipesLiked, favRecipe])
   }
   function saveForLater(selectedItem) {
-    removeSelectItemMainList(selectedItem)
     addRecipeSaved([...recipesSaved, selectedItem])
+    removeSelectItemMainList(selectedItem)
   }
   function selectRecipe(selectedItem) {
     if (selectedFood.length < 5) {
-      removeSelectItemMainList(selectedItem)
+      //TOTODODODOD!!!! the list will not remove the selected item, 
+      //
       updateSelectedFood([...selectedFood, selectedItem])
+      removeSelectItemMainList(selectedItem)
+      wklyRandomRecipe()
+      console.log(foodList)
     } else {
       alert("Too many recipes this week ")
     }
-
   }
 
   return (
@@ -72,5 +93,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
